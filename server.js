25720c6ea00e48c6ac33edd149e5b11b6bc681e6
Firmansyah Rozy <rozy.firmansyah@gmail.com>
2019-10-13 19:45:45 +0700

    update server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const cors = require("cors");
+const logger = require("morgan");
+const router = require("./src/routes/root");
+
+const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger("dev"));
+app.use(cors());
+app.use(helmet({ xssFilter: true }));
+
+app.use("api/", router);
+
+// running server
+const port = process.env.PORT || 6969;
+app.listen(port, () => console.log(`server listening on port ${port}`));
